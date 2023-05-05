18ff20b78408b1b1a60f1796a6be6d84cc72900d
James Hulett <54538579+jameshltt@users.noreply.github.com>
2023-05-05 15:52:04 +0200

    Update lambda_function.py
    Finalized function

--- lambda_function.py
+++ lambda_function.py
@@ -24,16 +24,19 @@ def lambda_handler(event, context):
     response = get_user_data(body)
     logger.info(f"Response from aggregator function: {response}")
     
-    text = "dummy-text"
-    
-    logging.info(f"Text: {text}")
-    logging.info("Calling generate_about_section")
-    about_text = generate_about_section(text)
-    logging.info(f"Response from get_user_data method:\n {about_text}")
-    
     return {
-        'statusCode': 200,
-        'body': json.dumps('about_text')
+        "statusCode": 200,
+        "body": {
+            "messageId": event['body']['messageId'],
+            "chatId": event['body']['chatId'],
+            "userId": event['body']['userId'],
+            "message": response,
+            "replyId": event['body']['replyId'],
+            "isCommand": event['body']['isCommand'],
+            "type": event['body']['type'],
+            "state": event['body']['state']
+  }
+
     }
 
 def get_user_data(body):
@@ -55,21 +58,59 @@ def get_user_data(body):
     )
     response_payload = json.loads(response['Payload'].read().decode("utf-8"))
     logger.info(f"Response payload from aggregator function: \n{response_payload}")
-    return response_payload
+    
+    payload_body = json.loads(response_payload['body'])
+    
+    logger.info("pulling user data from response payload")
+    user = payload_body['user']
+    logger.info(f"User: \n{user}")
+    
+    name = user['userName']
+    
+    # language
+    language = payload_body['lang'][0]
+    
+    # school
+    education = payload_body['edu'][0]
+    school = education['schoolName']
+    graduation_year = education['graduationYear']
+    achievements = education['achievementList']
+    
+    # experience
+    experience = payload_body['exp'][0]
+    last_company = experience['companyName']
+    role =  experience['role']
+    start_date = experience['dateStart']
+    end_date = experience['dateEnd']
+    
+    # skills
+    skills = payload_body['skill']
+    
+    list_skills = ''
+    for skill in skills:
+        list_skills += skill["skillName"] + ", "
+        
+    # Remove the last comma and space
+    list_skills = list_skills[:-2]
+    
+    logging.info("Calling generate_about_section")
+    about_section = generate_about_section(name, language, school, graduation_year, achievements, last_company, role, start_date, end_date, list_skills)
+    
+    return about_section
 
-def generate_about_section(text):
+def generate_about_section(name, language, school, graduation_year, achievements, last_company, role, start_date, end_date, list_skills):
     logging.info("In generate_about_section")
     # set openai api key
     openai.api_key = openai_api_key
     
-    prompt = f"Given the information about a user below, please generate a short (100 words maximum) 'about' section for a CV cover letter: {text}"
+    prompt = f"Given the information about a user below, please generate a short (300 words maximum) 'about' section for a CV cover letter. Name: {name}, language: {language}, school: {school}, graduation year: {graduation_year}, achievements in school: {achievements}, last company worked for: {last_company}, role in that company: {role}, start date in the role: {start_date}, end date in the role: {end_date} and a list of my skills: {list_skills}"
     response = openai.Completion.create(
         engine="text-davinci-003", 
         prompt=prompt,
         max_tokens=500,
         n=1,
         stop=None,
-        temperature=0.6,
+        temperature=0.65,
     )
     about = response.choices[0].text.strip()
     logging.info(f"About section:\n {about}")
